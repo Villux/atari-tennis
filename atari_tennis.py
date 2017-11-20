743248a9de9136b59bc031494e064247509ba05a
Ville Toiviainen <villej.toiviainen@gmail.com>
2017-11-20 10:49:09 +0200

    DQN with FF NN for cart pole This is done as an simple example to test RL algorithms before developing one with CNN

--- atari_tennis.py
+++ atari_tennis.py
@@ -1,45 +1,175 @@
 import gym
-from PIL import Image
+import math
+import random
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple
+from itertools import count
+from copy import deepcopy
+from PIL import Image
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torchvision.transforms as T
 
-def rgb2gray(image):
-    return image[:,:,0] * 299/1000 + image[:,:,1] * 587/1000 + image[:,:,2] * 114/1000
+env = gym.make('CartPole-v0')
 
-def prepro(I):
-    import ipdb; ipdb.set_trace()
-    # Keep only needed part of the image and make it a squere
-    I = I[48: 212, 10: 150]
-    # Downsample to (75, 75)
-    I = I[::2, ::2]
-    # Grayscale and return
-    return rgb2gray(I).ravel()
+# if gpu is to be used
+use_cuda = torch.cuda.is_available()
+FloatTensor = torch.cuda.FloatTensor if use_cuda else torch.FloatTensor
+LongTensor = torch.cuda.LongTensor if use_cuda else torch.LongTensor
+ByteTensor = torch.cuda.ByteTensor if use_cuda else torch.ByteTensor
+Tensor = FloatTensor
 
-def policy_forward(input):
-    return
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
 
-def policy_backwards():
-    return
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+
+    def push(self, *args):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+class Net(nn.Module):
+    def __init__(self, input_size, hidden_size, num_classes):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, num_classes)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        hidden = self.relu(self.fc1(x))
+        return self.sigmoid(self.fc2(hidden))
+
+model = Net(4, 30, 2)
+
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+
+optimizer = optim.Adam(model.parameters())
+memory = ReplayMemory(10000)
+
+steps_done = 0
+
+def select_action(state):
+    global steps_done
+    # Epsilon greedy with cool decay
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+
+    if sample > eps_threshold:
+        return model(Variable(state, volatile=True).type(FloatTensor)).data.max(0)[1].view(1, 1)
+    else:
+        return LongTensor([[random.randrange(2)]])
 
 
+episode_durations = []
 
+last_sync = 0
 
-env = gym.make('Tennis-v0')
-observation = env.reset()
+def optimize_model():
+    global last_sync
+    if len(memory) < BATCH_SIZE:
+        return
+    transitions = memory.sample(BATCH_SIZE)
+    batch = Transition(*zip(*transitions))
 
-for i in range(10000):
-    observation, reward, done, info = env.step(env.action_space.sample())
-    env.render()
-    prepro(observation)
+    # Binary mask
+    # Compute a mask of non-final states and concatenate the batch elements
+    non_final_mask = ByteTensor(tuple(map(lambda s: s is not None, batch.next_state)))
 
+    non_final_states = torch.stack([s for s in batch.next_state if s is not None])
+    non_final_next_states_tensor = Variable(non_final_states, volatile=True)
 
-img = Image.fromarray(I, 'RGB')
-img.show()
+    state_batch = Variable(torch.stack(batch.state))
+    action_batch = Variable(torch.cat(batch.action))
+    reward_batch = Variable(torch.cat(batch.reward))
+
+    # Compute Q(s_t, a) - select value by action index
+    state_action_values = model(state_batch).gather(1, action_batch)
+
+    # Compute V(s_{t+1}) for all next states.
+    next_state_values = Variable(torch.zeros(BATCH_SIZE).type(Tensor))
+    next_state_values[non_final_mask] = model(non_final_next_states_tensor).max(1)[0]
+    # Now, we don't want to mess up the loss with a volatile flag, so let's
+    # clear it. After this, we'll just end up with a Variable that has
+    # requires_grad=False
+    next_state_values.volatile = False
+    # Compute the expected Q values
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Compute Huber loss
+    loss = F.smooth_l1_loss(state_action_values, expected_state_action_values)
+
+    # Optimize the model
+    optimizer.zero_grad()
+    loss.backward()
+
+    # for param in model.parameters():
+    #     param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+
+def plot_durations():
+    plt.figure(2)
+    plt.clf()
+    durations_t = torch.FloatTensor(episode_durations)
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    # Take 100 episode averages and plot them too
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+# Run for 10 episodes
+num_episodes = 1000
+for i_episode in range(num_episodes):
+    state = torch.from_numpy(np.ascontiguousarray(env.reset(), dtype=np.float32))
+    for t in count():
+        # Select and perform an action
+        action = select_action(state)
+        next_state, reward, done, _ = env.step(action[0, 0])
+        next_state = torch.from_numpy(np.ascontiguousarray(next_state, dtype=np.float32))
+
+        if done:
+            next_state = None
+
+        # Store the transition in memory
+        memory.push(state, action, next_state, Tensor([reward]))
+
+        # Move to the next state
+        state = next_state
+
+        # Perform one step of the optimization (on the target network)
+        optimize_model()
+        if done:
+            episode_durations.append(t + 1)
+            plot_durations()
+            break
+
 env.close()
-
-
-
-# TODO
-# Policy forward
-# Stockhastic policy - which action to choose?
-
-# Policy backward

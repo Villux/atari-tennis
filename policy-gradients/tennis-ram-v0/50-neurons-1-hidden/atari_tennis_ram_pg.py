3dc109f06818d3f93fa4091dcda07d15f9403ede
Antti Partanen <antti.partanen@aalto.fi>
2017-12-03 22:31:26 +0200

    Created folders for different algorithms, gym environments and different neural network sizes

---
+++ policy-gradients/tennis-ram-v0/50-neurons-1-hidden/atari_tennis_ram_pg.py
@@ -0,0 +1,162 @@
+import argparse
+import numpy as np 
+from itertools import count
+
+# OpenaAi-Gym
+import gym
+# PyTorch utilities
+import torch
+import torch.nn as nn
+import torch.nn.functional as F 
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+import os.path
+
+resume = True
+
+parser = argparse.ArgumentParser(description='PyTorch implementation of OpenAi-Gym Atari 2600 Tennis-ram-v0')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G', help='discount factor (default: 0.99)')
+parser.add_argument('--seed', type=int, default=543, metavar='N', help='random seed (default: 543)')
+parser.add_argument('--render', action='store_true', help='render the environment')
+parser.add_argument('--log_interval', type=int, default=1, metavar='N', help='interval between training status logs (default: 10)')
+parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
+args = parser.parse_args()
+
+args.cuda = not args.disable_cuda and torch.cuda.is_available()
+args.cuda = False
+
+env = gym.make('Tennis-ram-v0')
+env.seed(args.seed)
+torch.manual_seed(args.seed)
+
+input_layer_size = 128
+output_layer_size = 18
+hidden_layer_size = 50
+
+
+class Policy(nn.Module):
+	def __init__(self):
+		super(Policy, self).__init__()
+		# Affine refers to affine transformation, which is a Linear transformation (f(x) = Ax + b) where the term b is 0.
+		# In neural networks the b denotes bias.
+
+		# In other words self.affine1 and self.affine2 are the weight matrices between the layers.
+		self.affine1 = nn.Linear(input_layer_size, hidden_layer_size)
+		self.affine2 = nn.Linear(hidden_layer_size, output_layer_size)
+
+		# Policy gradient needs to store actions and rewards to calculate the backpropagation
+		self.saved_actions = []
+		self.rewards = []
+
+	def forward(self, x):
+		# The input layer is multiplied by the first weight matrix self.affine1. 
+		# I.e the weight matrix between the input layer and the hidden layer.
+		# The activation function is a Rectifier. ReLu = Rectified Linear unit
+		# 
+		# One way ReLUs improve neural networks is by speeding up training. The gradient 
+		# computation is very simple (either 0 or 1 depending on the sign of x)
+		# Logistic and hyperbolic tangent activation functions suffer from vanishing gradient problem,  
+		# which makes learning increasingly slower. 
+		x = F.relu(self.affine1(x))
+
+		# The hidden layer is multiplied by the second weight matrix self.affine2.
+		# The activation function is softmax function, which is a sigmoid function, but 
+		# the terms of the vector are normalized so that the sum of the terms of the vector action_scores = 1.
+		action_scores = F.softmax(self.affine2(x))
+		return action_scores
+
+policy = Policy()
+
+episodes = []
+running_rewards = []
+
+if resume == True and os.path.exists("save_tennis_ram_model.p"):
+    policy.load_state_dict(torch.load("save_tennis_ram_model.p"))
+    if os.path.exists("save_tennis_ram_bookkeeping.p"):
+        #print("Bookkeeping found!")
+        temp = torch.load("save_tennis_ram_bookkeeping.p")
+        #print("Temp: {}".format(temp))
+        episodes = temp['episodes']
+        running_rewards = temp['running_rewards']
+        #print("Episodes: {}".format(episodes))
+        #print("Running rewards {}".format(running_rewards))
+    
+    
+optimizer = optim.Adam(policy.parameters(), lr=1e-2)
+
+if args.cuda:
+    policy.cuda()
+
+def select_action(state):
+	state = torch.from_numpy(state).float().unsqueeze(0)
+        if args.cuda: state = state.cuda()
+        probs = policy(Variable(state))
+
+	# Draw action from the probability distribution of the output. 
+	action = probs.multinomial()
+	policy.saved_actions.append(action)
+	return action.data
+
+
+def finish_episode():
+    R = 0
+    rewards = []
+    for r in policy.rewards[::-1]:
+        R = r + args.gamma * R
+        rewards.insert(0, R)
+
+    rewards = torch.Tensor(rewards)
+    if args.cuda: rewards = rewards.cuda()
+    rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
+    for action, r in zip(policy.saved_actions, rewards):
+        action.reinforce(r)
+    optimizer.zero_grad()
+    autograd.backward(policy.saved_actions, [None for _ in policy.saved_actions])
+    optimizer.step()
+    del policy.rewards[:]
+    del policy.saved_actions[:]
+
+
+
+running_reward = -1.0
+
+
+# Reward is only 1.0 when the tennis game is won, or -1.0 when the tennis is lost
+
+for i_episode in count(len(episodes)):
+		# State is the game observation. In this case the ram memory, which is only 1x128x(bytes)
+    state = env.reset()
+    final_reward = 0.0
+    # Start a new episode:
+    for t in range(30000): # Don't infinite loop while learning
+    	
+        # Forward propagation and action selection:
+        action = select_action(state)
+        # Evaluate the action
+        state, reward, done, _ = env.step(action[0,0])
+        if args.render:
+            env.render()
+
+        policy.rewards.append(reward)
+        if done:
+            #print("Game over!: Final reward: {}".format(reward))
+            final_reward = reward
+            break
+
+    running_reward = running_reward * 0.99 + final_reward * 0.01
+    episodes.append(i_episode)
+    running_rewards.append(running_reward)
+    #print("Running_reward: {}".format(running_reward))
+    finish_episode()
+
+    if i_episode % args.log_interval == 0:
+        print('Episode {}\t Timesteps: {:5d}\t Reward: {:.2f}\t Running reward: {:.2f}'.format(
+            i_episode, t, final_reward, running_reward))
+    if i_episode != 0 and i_episode % 100 == 0:
+        torch.save(policy.state_dict(), "save_tennis_ram_model.p")
+        torch.save({'running_rewards': running_rewards, 'episodes': episodes}, 'save_tennis_ram_bookkeeping.p')
+
+
+    

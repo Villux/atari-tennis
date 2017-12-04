ada251ca24a84eac2d536295a5172bde2dab2a48
Antti Partanen <anttipartanen10@gmail.com>
2017-12-04 03:14:52 +0200

    Modded the output of the tennis-ram-v0 files

--- policy-gradients/tennis-ram-v0/50-neurons-1-hidden/atari_tennis_ram_pg.py
+++ policy-gradients/tennis-ram-v0/50-neurons-1-hidden/atari_tennis_ram_pg.py
@@ -33,55 +33,56 @@ torch.manual_seed(args.seed)
 
 input_layer_size = 128
 output_layer_size = 18
-hidden_layer_size = 50
+hidden_layer1_size = 50
+hidden_layer2_size = 50
 
 
 class Policy(nn.Module):
-	def __init__(self):
-		super(Policy, self).__init__()
-		# Affine refers to affine transformation, which is a Linear transformation (f(x) = Ax + b) where the term b is 0.
-		# In neural networks the b denotes bias.
+    def __init__(self):
+        super(Policy, self).__init__()
+        # Affine refers to affine transformation, which is a Linear transformation (f(x) = Ax + b) where the term b is 0.
+        # In neural networks the b denotes bias.
 
-		# In other words self.affine1 and self.affine2 are the weight matrices between the layers.
-		self.affine1 = nn.Linear(input_layer_size, hidden_layer_size)
-		self.affine2 = nn.Linear(hidden_layer_size, output_layer_size)
+        # In other words self.affine1 and self.affine2 are the weight matrices between the layers.
+        self.affine1 = nn.Linear(input_layer_size, hidden_layer1_size)
+        self.affine2 = nn.Linear(hidden_layer1_size, hidden_layer2_size)        
+        self.affine3 = nn.Linear(hidden_layer2_size, output_layer_size)
 
-		# Policy gradient needs to store actions and rewards to calculate the backpropagation
-		self.saved_actions = []
-		self.rewards = []
+        # Policy gradient needs to store actions and rewards to calculate the backpropagation
+        self.saved_actions = []
+        self.rewards = []
 
-	def forward(self, x):
-		# The input layer is multiplied by the first weight matrix self.affine1. 
-		# I.e the weight matrix between the input layer and the hidden layer.
-		# The activation function is a Rectifier. ReLu = Rectified Linear unit
-		# 
-		# One way ReLUs improve neural networks is by speeding up training. The gradient 
-		# computation is very simple (either 0 or 1 depending on the sign of x)
-		# Logistic and hyperbolic tangent activation functions suffer from vanishing gradient problem,  
-		# which makes learning increasingly slower. 
-		x = F.relu(self.affine1(x))
+    def forward(self, x):
+        # The input layer is multiplied by the first weight matrix self.affine1. 
+        # I.e the weight matrix between the input layer and the hidden layer.
+        # The activation function is a Rectifier. ReLu = Rectified Linear unit
+        # 
+        # One way ReLUs improve neural networks is by speeding up training. The gradient 
+        # computation is very simple (either 0 or 1 depending on the sign of x)
+        # Logistic and hyperbolic tangent activation functions suffer from vanishing gradient problem,  
+        # which makes learning increasingly slower. 
+        
+        x = F.relu(self.affine1(x))
+        x = F.relu(self.affine2(x)) 
 
-		# The hidden layer is multiplied by the second weight matrix self.affine2.
-		# The activation function is softmax function, which is a sigmoid function, but 
-		# the terms of the vector are normalized so that the sum of the terms of the vector action_scores = 1.
-		action_scores = F.softmax(self.affine2(x))
-		return action_scores
+        # The hidden layer is multiplied by the second weight matrix self.affine2.
+        # The activation function is softmax function, which is a sigmoid function, but 
+        # the terms of the vector are normalized so that the sum of the terms of the vector action_scores = 1.
+        action_scores = F.softmax(self.affine3(x))
+        return action_scores
 
 policy = Policy()
-
 episodes = []
 running_rewards = []
+running_reward = None
 
 if resume == True and os.path.exists("save_tennis_ram_model.p"):
     policy.load_state_dict(torch.load("save_tennis_ram_model.p"))
     if os.path.exists("save_tennis_ram_bookkeeping.p"):
-        #print("Bookkeeping found!")
         temp = torch.load("save_tennis_ram_bookkeeping.p")
-        #print("Temp: {}".format(temp))
         episodes = temp['episodes']
         running_rewards = temp['running_rewards']
-        #print("Episodes: {}".format(episodes))
-        #print("Running rewards {}".format(running_rewards))
+        running_rewad = running_rewards[-1]
     
     
 optimizer = optim.Adam(policy.parameters(), lr=1e-2)
@@ -90,14 +91,14 @@ if args.cuda:
     policy.cuda()
 
 def select_action(state):
-	state = torch.from_numpy(state).float().unsqueeze(0)
-        if args.cuda: state = state.cuda()
-        probs = policy(Variable(state))
+    state = torch.from_numpy(state).float().unsqueeze(0)
+    if args.cuda: state = state.cuda()
+    probs = policy(Variable(state))
 
-	# Draw action from the probability distribution of the output. 
-	action = probs.multinomial()
-	policy.saved_actions.append(action)
-	return action.data
+    # Draw action from the probability distribution of the output. 
+    action = probs.multinomial()
+    policy.saved_actions.append(action)
+    return action.data
 
 
 def finish_episode():
@@ -119,44 +120,48 @@ def finish_episode():
     del policy.saved_actions[:]
 
 
-
-running_reward = -1.0
-
-
-# Reward is only 1.0 when the tennis game is won, or -1.0 when the tennis is lost
-
-for i_episode in count(len(episodes)):
-		# State is the game observation. In this case the ram memory, which is only 1x128x(bytes)
-    state = env.reset()
-    final_reward = 0.0
-    # Start a new episode:
-    for t in range(30000): # Don't infinite loop while learning
-    	
-        # Forward propagation and action selection:
-        action = select_action(state)
-        # Evaluate the action
-        state, reward, done, _ = env.step(action[0,0])
-        if args.render:
-            env.render()
-
-        policy.rewards.append(reward)
-        if done:
-            #print("Game over!: Final reward: {}".format(reward))
-            final_reward = reward
-            break
-
-    running_reward = running_reward * 0.99 + final_reward * 0.01
-    episodes.append(i_episode)
-    running_rewards.append(running_reward)
-    #print("Running_reward: {}".format(running_reward))
-    finish_episode()
-
-    if i_episode % args.log_interval == 0:
-        print('Episode {}\t Timesteps: {:5d}\t Reward: {:.2f}\t Running reward: {:.2f}'.format(
-            i_episode, t, final_reward, running_reward))
-    if i_episode != 0 and i_episode % 100 == 0:
-        torch.save(policy.state_dict(), "save_tennis_ram_model.p")
-        torch.save({'running_rewards': running_rewards, 'episodes': episodes}, 'save_tennis_ram_bookkeeping.p')
+state = env.reset()
 
 
-    
+
+reward_sum = 0
+episode_number = len(episodes)
+frame = 0
+
+while True:
+    if args.render: env.render()
+
+    action = select_action(state)
+    state, reward, done, _ = env.step(action[0,0])
+    reward_sum += reward
+    policy.rewards.append(reward)
+    frame += 1
+
+
+    if done: 
+        episode_number += 1
+        finish_episode()
+
+        if running_reward is None:
+            running_reward = reward_sum
+        else:
+            running_reward = 0.99 * running_reward + 0.01 * reward_sum
+
+        print("Episode: {}. Resetting the environment. Episode reward total was {}. Running mean: {}".format(episode_number, reward_sum, running_reward))
+
+        episodes.append(episode_number)
+        running_rewards.append (running_reward)
+
+        if episode_number != 0 and episode_number % 100 == 0:
+            torch.save(policy.state_dict(), "save_tennis_ram_model.p")
+            torch.save({'running_rewards': running_rewards, 'episodes': episodes}, 'save_tennis_ram_bookkeeping.p')
+
+        reward_sum = 0
+        frame = 0
+        state = env.reset()
+
+
+
+    if reward != 0:
+        print("Ep. {}. Frame: {}. Round finished!, reward: {}".format(episode_number, frame, reward))
+

22ad65e37fb04e0f9ad2174cd5224eff71c4392b
Antti Partanen <anttipartanen10@gmail.com>
2017-12-22 10:20:06 +0200

    Updated the cartpole policy gradient code

--- policy-gradients/cartpole-v0/cartpole-pg.py
+++ policy-gradients/cartpole-v0/cartpole-pg.py
@@ -1,7 +1,8 @@
-import argparse
-import gym
+#!/usr/bin/env python
+
 import numpy as np
-from itertools import count
+import gym
+import argparse
 
 import torch
 import torch.nn as nn
@@ -14,24 +15,19 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
-parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
-                    help='discount factor (default: 0.99)')
-parser.add_argument('--seed', type=int, default=543, metavar='N',
-                    help='random seed (default: 543)')
-parser.add_argument('--render', action='store_true',
-                    help='render the environment')
-parser.add_argument('--log_interval', type=int, default=10, metavar='N',
-                    help='interval between training status logs (default: 10)')
-parser.add_argument('--neurons', type=int, default=128, metavar='N',
-                    help="Number of neurons in the hidden layer")
+seed = 123
+
+parser = argparse.ArgumentParser(description='OpenAi Gym cartpole with policy gradients')
+
+parser.add_argument('--gamma', type=float, default=0.99, metavar='g', help='discount factor for rewards')
+parser.add_argument('--render', action='store_true', help='renders the game')
+parser.add_argument('--neurons', type=int, default=100, metavar='N', help="Number of neurons in the hidden layer")
 args = parser.parse_args()
 
 print("Running the cartpole, with {} neurons and with discount rate of {}".format(args.neurons, args.gamma))
 
-env = gym.make('CartPole-v0')
-env.seed(args.seed)
-torch.manual_seed(args.seed)
+torch.manual_seed(seed)
+
 max_episodes = 5000
 
 input_layer_size = 4
@@ -39,88 +35,106 @@ hidden_layer_size = args.neurons
 output_layer_size = 2
 
 
-class Policy(nn.Module):
+class NeuralNetwork(nn.Module):
     def __init__(self):
-        super(Policy, self).__init__()
-        self.affine1 = nn.Linear(input_layer_size, hidden_layer_size)
-        self.affine2 = nn.Linear(hidden_layer_size, output_layer_size)
+        super(NeuralNetwork, self).__init__()
+        # Single layered network:
+        # W1 is the weights between input and hidden layer
+        # W2 is the weights between hidden layer and output
+        self.W1 = nn.Linear(input_layer_size, hidden_layer_size)
+        self.W2 = nn.Linear(hidden_layer_size, output_layer_size)
 
-        self.saved_actions = []
+        self.actions = []
         self.rewards = []
 
     def forward(self, x):
-        x = F.relu(self.affine1(x))
-        action_scores = self.affine2(x)
-        return F.softmax(action_scores)
+        return F.softmax(self.W2(F.relu(self.W1(x))))
 
 
-policy = Policy()
-optimizer = optim.Adam(policy.parameters(), lr=1e-2)
+policy_nn = NeuralNetwork()
+optimizer = optim.Adam(policy_nn.parameters(), lr=1e-2)
 
 
-def select_action(state):
+def update_weights():
+    reward = 0
+    rewards = []
+    for r in policy_nn.rewards[::-1]:
+        reward = r + args.gamma * reward
+        rewards.insert(0, reward)
+    rewards = torch.Tensor(rewards)
+    
+    # Standardize the rewards
+    rewards -= rewards.mean()
+    rewards /= rewards.std() + np.finfo(np.float32).eps
+
+    for action, r in zip(policy_nn.actions, rewards):
+        action.reinforce(r)
+
+    optimizer.zero_grad()
+    autograd.backward(policy_nn.actions, [None for _ in policy_nn.actions])
+    optimizer.step()
+    
+    del policy_nn.rewards[:]
+    del policy_nn.actions[:]
+
+
+
+def sample_action(state):
     state = torch.from_numpy(state).float().unsqueeze(0)
-    probs = policy(Variable(state))
-    action = probs.multinomial()
-    policy.saved_actions.append(action)
+    # Get the probability distribution of actions
+    action_probabilities = policy_nn(Variable(state))
+    # Sample that distribution
+    action = action_probabilities.multinomial()
+    policy_nn.actions.append(action)
     return action.data
 
+env = gym.make('CartPole-v0')
+env.seed(seed)
 
-def finish_episode():
-    R = 0
-    rewards = []
-    for r in policy.rewards[::-1]:
-        R = r + args.gamma * R
-        rewards.insert(0, R)
-    rewards = torch.Tensor(rewards)
-    rewards = (rewards - rewards.mean()) / (rewards.std() + np.finfo(np.float32).eps)
-    for action, r in zip(policy.saved_actions, rewards):
-        action.reinforce(r)
-    optimizer.zero_grad()
-    autograd.backward(policy.saved_actions, [None for _ in policy.saved_actions])
-    optimizer.step()
-    del policy.rewards[:]
-    del policy.saved_actions[:]
+episodes_list = []
+running_average_episode_length_list = []
+episode_length_list = []
+running_average_episode_length = 10
 
-episodes = []
-average_timesteps = []
-timesteps = []
-running_reward = 10
-
-for i_episode in range(0, max_episodes):
+for episode in range(1, max_episodes):
+    
     state = env.reset()
-    for t in range(10000):
-        action = select_action(state)
+
+    for episode_length in range(1000):
+        action = sample_action(state)
         state, reward, done, _ = env.step(action[0,0])
+        
         if args.render:
             env.render()
-        policy.rewards.append(reward)
+
+        policy_nn.rewards.append(reward)
+
         if done:
             break
-    timesteps.append(t)
-    running_reward = running_reward * 0.99 + t * 0.01
 
-    episodes.append(i_episode)
-    average_timesteps.append(running_reward)
+    episode_length_list.append(episode_length)
+    running_average_episode_length = running_average_episode_length * 0.99 + episode_length * 0.01
 
-    finish_episode()
-    if i_episode % args.log_interval == 0:
-        print('Episode {}\tLast length: {:5d}\tAverage length: {:.2f}'.format(
-            i_episode, t, running_reward))
-    if running_reward > env.spec.reward_threshold:
-        print("Solved! Running reward is now {} and "
-              "the last episode runs to {} time steps!".format(running_reward, t))
+    episodes_list.append(episode)
+    running_average_episode_length_list.append(running_average_episode_length)
+
+    update_weights()
+
+    if episode % 10 == 0:
+        print('Episode number: {} Length: {} Running average length: {}'.format(episode, episode_length, running_average_episode_length))
+    if running_average_episode_length > env.spec.reward_threshold:
+        print("Game solved! Reached the reward threshold, with running average length: {}. Final episode length: {}".format(running_average_episode_length, episode_length))
         break
 
 
-plt.plot(episodes, average_timesteps, label="Running average length", )
-plt.scatter(episodes, timesteps, label="length", color="r")
+plt.plot(episodes_list, running_average_episode_length_list, label="Running average length", )
+plt.scatter(episodes_list, episode_length_list, label="length", color="r")
 
 plt.ylabel("Length")
 plt.xlabel("Episodes")
-plt.title("Discount: {}. Hidden layers: 1. Hidden neurons: {}.\n Total episodes: {}".format(args.gamma, hidden_layer_size, len(episodes)))
+plt.title("Discount: {}. Hidden layers: 1. Hidden neurons: {}.\n Total episodes: {}".format(args.gamma, hidden_layer_size, len(episodes_list)))
 plt.legend()
-plt.savefig("plots/cartpole_pg_1_layer_hidden_neurons_{}_gamma_{}_episodes_{}.png".format(hidden_layer_size, args.gamma, len(episodes)))
+plt.savefig("plots/cartpole_pg_1_layer_hidden_neurons_{}_gamma_{}_episodes_{}.png".format(hidden_layer_size, args.gamma, len(episodes_list)))
 
 
 
